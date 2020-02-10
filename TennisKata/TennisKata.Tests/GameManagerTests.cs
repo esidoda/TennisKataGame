923de4708d7886c584f6bbc399cbcabff774f32e
esidoda <esidoda@gmail.com>
2020-02-10 19:21:05 +0100

    Created Game Manager class, responsible for the main functionality of the game. Created and runned unit tests related with its constructor.

---
+++ TennisKata/TennisKata.Tests/GameManagerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TennisKata.Abstract;
+using TennisKata.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using TennisKata.Models;
+
+namespace TennisKata.Testing
+{
+    /// <summary>
+    /// Class responsible to methods related with GameManager class.
+    /// </summary>
+    [TestClass]
+    public class GameManagerTests
+    {
+        /// <summary>
+        /// Checks if the excepted exception is throwed when console service is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GameManager_ConsoleServiceNull()
+        {
+            ServiceProvider serviceProvider = ServiceHelper.RegisterServices();
+            // Get service provider
+            IConsoleService consoleService = null;
+            IPlayerService playerService = serviceProvider.GetService<IPlayerService>();
+            IPointService pointService = serviceProvider.GetService<IPointService>();
+            //Play tennis game
+            GameManager game = new GameManager(consoleService, playerService, pointService);
+        }
+
+        /// <summary>
+        /// Checks if the excepted exception is throwed when player service is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GameManager_PlayerServiceNull()
+        {
+            ServiceProvider serviceProvider = ServiceHelper.RegisterServices();
+            // Get service provider
+            IConsoleService consoleService = serviceProvider.GetService<IConsoleService>();
+            IPlayerService playerService = null;
+            IPointService pointService = serviceProvider.GetService<IPointService>();
+            //Play tennis game
+            GameManager game = new GameManager(consoleService, playerService, pointService);
+        }
+
+        /// <summary>
+        /// Checks if the excepted exception is throwed when point service is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GameManager_PointServiceNull()
+        {
+            ServiceProvider serviceProvider = ServiceHelper.RegisterServices();
+            // Get service provider
+            IConsoleService consoleService = serviceProvider.GetService<IConsoleService>();
+            IPlayerService playerService = serviceProvider.GetService<IPlayerService>();
+            IPointService pointService = null;
+            //Play tennis game
+            GameManager game = new GameManager(consoleService, playerService, pointService);
+        }
+    }
+}
---
+++ TennisKata/TennisKata/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TennisKata.Abstract;
+using TennisKata.Models;
+
+namespace TennisKata
+{
+    /// <summary>
+    /// Class to manage the game.
+    /// </summary>
+    public class GameManager
+    {
+        #region Fields
+        private readonly IConsoleService consoleService;
+        private readonly IPlayerService playerService;
+        private readonly IPointService pointService;
+        private Player playerA;
+        private Player playerB;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="GameManager"/> class.
+        /// </summary>
+        /// <param name="consoleService">The console service that will be used by the class.</param>
+        /// <param name="playerService">The player service that will be used by the class.</param>
+        /// <param name="pointService">The point service that will be used by the class.</param>
+        public GameManager(IConsoleService consoleService, IPlayerService playerService, IPointService pointService)
+        {
+            this.consoleService = consoleService ?? throw new ArgumentNullException("consoleService");
+            this.playerService = playerService ?? throw new ArgumentNullException("playerService");
+            this.pointService = pointService ?? throw new ArgumentNullException("pointService");
+        }
+
+        #endregion
+    }
+}
